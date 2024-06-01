3f3163c03f55d576bd844d209249704988a19490
minseok1897 <msj8719@naver.com>
2024-06-01 15:32:10 +0900

    User plane addition and movement implementation

--- main.py
+++ main.py
@@ -10,9 +10,19 @@ BLACK     = (  0,   0,   0)
 SKYBLUE   = (153, 255, 255)
 DARKBLUE  = (  0,  51, 255)
 
-BGCOLOR = BLACK
 
 done= False
+UP = 'up'
+DOWN = 'down'
+LEFT = 'left'
+RIGHT = 'right'
+
+airplane1 = pygame.image.load('./images/airplane1.png')
+airplane2 = pygame.image.load('./images/airplane2.png')
+airplane3 = pygame.image.load('./images/airplane3.png')
+airplane4 = pygame.image.load('./images/airplane4.png')
+airplane = pygame.transform.scale(airplane1, (100, 100))
+
 
 def main():
     global FPSCLOCK, DISPLAYSURF, BASICFONT
@@ -30,14 +40,32 @@ def main():
 
 
 def runGame():
-    global done
+    global airplane1, airplane2, airplane3, airplane4, airplane
+    x=270
+    y= HEIGHT-120
+
     while not done:
-        FPSCLOCK.tick(10)
-        DISPLAYSURF.fill(BLACK)
+        FPSCLOCK.tick(50)
+        DISPLAYSURF.fill(SKYBLUE)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 terminate()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_UP:
+                    airplane = pygame.transform.scale(airplane1, (100, 100))
+                    y-=20
+                elif event.key ==pygame.K_DOWN:
+                    airplane = pygame.transform.scale(airplane3, (100, 100))
+                    y+=20
+                elif event.key ==pygame.K_RIGHT:
+                    airplane = pygame.transform.scale(airplane2, (100, 100))
+                    x+=20
+                elif event.key ==pygame.K_LEFT:
+                    airplane = pygame.transform.scale(airplane4, (100, 100))
+                    x-=20
+        DISPLAYSURF.blit(airplane,(x,y))
+        pygame.display.update()
 
 
 def drawPressKeyMsg():
@@ -66,7 +94,7 @@ def showStartScreen():
 
     degrees1 = 0
     while True:
-        DISPLAYSURF.fill(BGCOLOR)
+        DISPLAYSURF.fill(BLACK)
         
         rotatedSurf1 = pygame.transform.rotate(titleSurf1, degrees1)
         rotatedRect1 = rotatedSurf1.get_rect()
